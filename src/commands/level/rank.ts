83024ea6b4e95c964e7cd91d8ac186fe4951f886
bowenwaregames <ttbowen@hotmail.co.uk>
2018-04-08 11:45:00 +0100

    Add command argument for fetching other guild members rank.

--- src/commands/level/rank.ts
+++ src/commands/level/rank.ts
@@ -1,10 +1,14 @@
-import { RichEmbed } from 'discord.js';
-import { Command, Message } from 'yamdbf';
+import { GuildMember, RichEmbed } from 'discord.js';
+import { Command, CommandDecorators, Message, Middleware } from 'yamdbf';
 import { BotClient } from '../../client/botClient';
 import { Database } from '../../database/database';
+import User from '../../database/models/User';
 import UserExpStats from '../../database/models/UserExpStats';
 import { LevelManager } from '../../managers/levelManager';
 import { Player } from '../../types/player';
+
+const { resolve } = Middleware;
+const { using } = CommandDecorators;
 
 export default class extends Command<BotClient> {
     constructor() {
@@ -17,20 +21,25 @@ export default class extends Command<BotClient> {
         });
     }
 
-    async action(message: Message): Promise<any> {
+    @using(resolve('member?: Member'))
+    async action(message: Message, [member]: [GuildMember]): Promise<any> {
         const enabled = await message.guild.storage.settings.get('levels');
         if (!enabled) return;
 
         try {
             const guildId = message.guild.id;
-            const userId = message.author.id;
-            const user: UserExpStats = await Database.db.models.UserExpStats.findOne({
+            let userId;
+
+            if (!member) userId = message.author.id;
+            else userId = member.id;
+
+            const userStats: UserExpStats = await Database.db.models.UserExpStats.findOne({
                 where: { guildId: guildId, userId: userId }
             });
             const players: UserExpStats[] = await Database.db.models.UserExpStats.findAll({
                 where: { guildId: guildId }
             });
-            const level = LevelManager.getLevelFromExp(user.exp);
+            const level = LevelManager.getLevelFromExp(userStats.exp);
 
             let xp = 0;
             for (let i = 0; i < level; i++) {
@@ -39,12 +48,16 @@ export default class extends Command<BotClient> {
             const playerSorted = players.sort((a, b) => a.exp - b.exp).reverse();
 
             const info: Player = {
-                totalExp: user.exp,
+                totalExp: userStats.exp,
                 levelExp: LevelManager.levelToExp(level),
-                remainingExp: user.exp - xp,
+                remainingExp: userStats.exp - xp,
                 level: level,
                 rank: playerSorted.findIndex(p => p.userId === userId) + 1
             };
+
+            const user: User = await Database.db.models.User.findOne({
+                where: { id: userId }
+            });
 
             const colour = 7911109;
             const embed = new RichEmbed()
@@ -53,7 +66,7 @@ export default class extends Command<BotClient> {
                 .addField(`Lvl Exp`, `${info.remainingExp}/${info.levelExp}`, true)
                 .addField(`Total Exp`, `${info.totalExp}`, true)
                 .setColor(colour)
-                .setAuthor(message.author.username, message.author.avatarURL);
+                .setAuthor(user.username, user.avatarUrl);
 
             return message.channel.send('', { embed: embed });
         } catch {
