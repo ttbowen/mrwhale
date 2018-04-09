c93f49398b579474b1fad9d78dab202ef5569a67
bowenwaregames <ttbowen@hotmail.co.uk>
2018-04-09 01:43:13 +0100

    Added pastebin command.

---
+++ src/commands/useful/pastebin.ts
@@ -0,0 +1,44 @@
+import { Client, Command, Message } from 'yamdbf';
+import { BotClient } from '../../client/botClient';
+
+import * as request from 'request-promise';
+
+export default class extends Command<BotClient> {
+    constructor() {
+        super({
+            name: 'pastebin',
+            desc: 'Upload a paste to pastebin.',
+            usage: '<prefix>pastebin <paste>',
+            group: 'useful',
+            aliases: ['paste', 'pb']
+        });
+    }
+
+    async action(message: Message): Promise<any> {
+        const prefix: string =
+            (await message.guild.storage.settings.get('prefix')) ||
+            (await this.client.storage.get('prefix'));
+
+        const paste: string = message.content.replace(`${prefix}${this.name}`, '');
+
+        if (!await message.guild.storage.settings.get('pastebin'))
+            return message.channel.send('No API key provided for pastebin.');
+
+        if (!paste || paste === '')
+            return message.channel.send('Please provide a paste to upload.');
+
+        const options = {
+            url: `https://pastebin.com/api/api_post.php`,
+            method: 'POST',
+            form: {
+                api_option: 'paste',
+                api_paste_code: paste,
+                api_dev_key: await message.guild.storage.settings.get('pastebin')
+            }
+        };
+
+        return request(options).then(body => {
+            if (body) return message.channel.send(body);
+        });
+    }
+}
