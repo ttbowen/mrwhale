7d1db76dc5b9a42a13e0351fd00a9ff6b18ce009
bowenwaregames <ttbowen@hotmail.co.uk>
2018-04-19 00:00:01 +0100

    Added more aliases and overrides for conchshell answers.

--- src/commands/fun/8ball.ts
+++ src/commands/fun/8ball.ts
@@ -8,16 +8,27 @@ export default class extends Command<BotClient> {
             name: '8ball',
             desc: 'A magic 8ball. Tells your fortune.',
             usage: '<prefix>8ball',
-            aliases: ['conchshell'],
+            aliases: ['conch', 'conchshell', 'magicconch', 'magicconchshell'],
             group: 'fun'
         });
     }
 
     async action(message: Message): Promise<any> {
-        if (message.content.match(/(will\si\s(?:ever)?\s*get\smarried(\?*))/gi))
-            return message.channel.send(`:8ball: Maybe someday.`);
+        let index = Math.floor(Math.random() * responses.default.length);
 
-        const index = Math.floor(Math.random() * responses.default.length);
+        if (message.content.match(/conchshell/)) {
+            const conchShellOverrides = [`I don't think so.`, `Yes.`, `Try asking again.`, `No.`];
+
+            index = Math.floor(Math.random() * conchShellOverrides.length);
+
+            if (message.content.match(/w(?:o|u|ha)t\s(?:do|to|(?:sh|w)ould)[\s\S]*/gi))
+                return message.channel.send(':shell: Nothing.');
+
+            if (message.content.match(/(will\si\s(?:ever)?\s*get\smarried(\?*))/gi))
+                return message.channel.send(`:shell: Maybe someday.`);
+
+            return message.channel.send(`:shell: ${conchShellOverrides[index]}`);
+        }
 
         return message.channel.send(`:8ball: ${responses.default[index]}`);
     }
