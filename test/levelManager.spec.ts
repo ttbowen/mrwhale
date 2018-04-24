eabd87801b5957103979b2bc61d06ffb1bf4323f
bowenwaregames <ttbowen@hotmail.co.uk>
2018-04-24 23:30:32 +0100

    Grouped LevelManager unit tests.

--- test/levelManager.spec.ts
+++ test/levelManager.spec.ts
@@ -1,20 +1,22 @@
 import { expect } from 'chai';
 import { LevelManager } from '../src/client/managers/levelManager';
 
-describe('levelToExp', () => {
-    it('should calculate the correct experience from level', () => {
-        const expectedExp = 155;
-        const level = 1;
+describe('LevelManager', () => {
+    describe('levelToExp', () => {
+        it('should calculate the correct experience from level', () => {
+            const expectedExp = 155;
+            const level = 1;
 
-        expect(LevelManager.levelToExp(level)).to.equal(expectedExp);
+            expect(LevelManager.levelToExp(level)).to.equal(expectedExp);
+        });
     });
-});
 
-describe('getLevelFromExp', () => {
-    it('should calculate the correct level from the given experience', () => {
-        const expectedLevel = 1;
-        const experience = 155;
+    describe('getLevelFromExp', () => {
+        it('should calculate the correct level from the given experience', () => {
+            const expectedLevel = 1;
+            const experience = 155;
 
-        expect(LevelManager.getLevelFromExp(experience)).to.equal(expectedLevel);
+            expect(LevelManager.getLevelFromExp(experience)).to.equal(expectedLevel);
+        });
     });
 });
