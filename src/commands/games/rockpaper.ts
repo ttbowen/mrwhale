4993f761cc0c6de6876cce6415ed498fb96a613a
bowenwaregames <ttbowen@hotmail.co.uk>
2018-04-28 16:29:43 +0100

    Added rockpaper command.

---
+++ src/commands/games/rockpaper.ts
@@ -0,0 +1,54 @@
+import { Command, Message } from 'yamdbf';
+import { BotClient } from '../../client/botClient';
+
+export default class extends Command<BotClient> {
+    constructor() {
+        super({
+            name: 'rockpaper',
+            desc: 'Rock. Paper. Scissors.',
+            usage: '<prefix>rockpaper <rock|paper|scissors>',
+            aliases: ['rps'],
+            group: 'games'
+        });
+    }
+
+    private compare(first: string, second: string): string {
+        if (first === second) {
+            return 'It\'s a tie!';
+        } else if (first === 'scissors') {
+            if (second === 'paper') return 'Scissors wins! :v:';
+            else return 'Rock wins! :fist:';
+        } else if (first === 'rock') {
+            if (second === 'scissors') return 'Rock wins! :fist:';
+            else return 'Paper wins! :raised_hand:';
+        } else if (first === 'paper') {
+            if (second === 'rock') return 'Paper wins! :raised_hand:';
+            else return 'Scissors wins! :v:';
+        }
+    }
+
+    async action(message: Message, [choice]: [string]): Promise<any> {
+        if (!choice || choice === '') return message.channel.send(`Please pass a choice.`);
+
+        const userChoice: string = choice.trim().toLowerCase();
+        const compChoice: number = Math.random();
+        let compChoiceStr = '';
+
+        const validChoices: RegExp = /(rock|paper|scissors)/;
+
+        if (!message.content.match(validChoices))
+            return message.channel.send('Please pass rock, paper, scissors.');
+
+        const rockVal = 0.34;
+        if (compChoice < 0.34) {
+            compChoiceStr = 'rock';
+        } else if (compChoice <= 0.67) {
+            compChoiceStr = 'paper';
+        } else {
+            compChoiceStr = 'scissors';
+        }
+        const result: string = this.compare(userChoice, compChoiceStr);
+
+        return message.channel.send(`${compChoiceStr}. ${result}`);
+    }
+}
