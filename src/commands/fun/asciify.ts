f73c31485316745dc7e027ec236a44a962474ff3
bowenwaregames <ttbowen@hotmail.co.uk>
2018-05-12 16:31:47 +0100

    Refactored asciify.

--- src/commands/fun/asciify.ts
+++ src/commands/fun/asciify.ts
@@ -1,8 +1,11 @@
 import { RichEmbed } from 'discord.js';
 import * as request from 'request-promise';
-import { Command, Message } from 'yamdbf';
+import { Command, CommandDecorators, Message, Middleware } from 'yamdbf';
 import { BotClient } from '../../client/botClient';
 import asciify, * as fontList from '../../data/asciify';
+
+const { resolve, expect } = Middleware;
+const { using } = CommandDecorators;
 
 export default class extends Command<BotClient> {
     constructor() {
@@ -12,7 +15,7 @@ export default class extends Command<BotClient> {
             usage: '<prefix>asciify <font index> <text>',
             aliases: ['graffiti'],
             group: 'fun',
-            ratelimit: '2/1m'
+            ratelimit: '5/1m'
         });
     }
 
@@ -20,58 +23,26 @@ export default class extends Command<BotClient> {
         let formattedAscii = '';
         let currentIndex = 0;
         do {
-            if (text[currentIndex] === '\n') {
-                formattedAscii += '\n';
-            } else if (text[currentIndex] === ' ') {
-                formattedAscii += '░';
-            } else {
-                formattedAscii += '▓';
-            }
+            if (text[currentIndex] === '\n') formattedAscii += '\n';
+            else if (text[currentIndex] === ' ') formattedAscii += '░';
+            else formattedAscii += '▓';
+
             currentIndex++;
         } while (currentIndex < text.length);
         return formattedAscii;
     }
 
-    async action(message: Message, inputs: string[]): Promise<any> {
-        const fontID = +inputs[0];
-        if (Number.isNaN(fontID)) {
-            return message.channel.send(
-                'No valid number given. Please pass in a valid number for font id'
-            );
-        }
-        if (fontID >= fontList.default.length) {
-            return message.channel.send(
-                `Please insert a number lower than ${fontList.default.length.toString()}`
-            );
-        }
+    @using(resolve('fontID: Number, ...text: String'))
+    @using(expect('fontID: Number, ...text: String'))
+    async action(message: Message, [fontID, ...text]: [number, string[]]): Promise<any> {
+        const fonts: string[] = fontList.default;
+        if (fontID >= fonts.length) fontID = fonts.length - 1;
+        if (fontID < 0) fontID = 0;
 
-        if (inputs.length === 1) {
-            return message.channel.send(
-                'No text is inserted. Or format is wrong. Use help command to see the format'
-            );
-        }
-
-        let textsToAsciify = '';
-
-        for (const index in inputs) {
-            if (+index !== 0) {
-                let loop = 0;
-                do {
-                    textsToAsciify += encodeURI(inputs[index][loop]);
-                    loop++;
-                } while (loop < inputs[index].length);
-                if (+index < inputs.length - 1) {
-                    textsToAsciify += '+';
-                }
-            }
-        }
-
+        const textsToAsciify = `${encodeURI(text.join('+'))}`;
         const options = {
             url:
-                'http://artii.herokuapp.com/make?font=' +
-                fontList.default[fontID] +
-                '&text=' +
-                textsToAsciify,
+                'http://artii.herokuapp.com/make?font=' + fonts[fontID] + '&text=' + textsToAsciify,
             method: 'GET'
         };
 
