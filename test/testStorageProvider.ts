0140e59186c3a7d4ccb869197da923c493c2d7d8
bowenwaregames <ttbowen@hotmail.co.uk>
2018-05-18 00:16:36 +0100

    Add test storage provider.

---
+++ test/testStorageProvider.ts
@@ -0,0 +1,32 @@
+import { StorageProvider, StorageProviderConstructor } from 'yamdbf';
+
+/**
+ * In-memory storage provider for testing.
+ */
+export function TestStorageProvider(): StorageProviderConstructor {
+    return class extends StorageProvider {
+        private _storage: any;
+
+        constructor() {
+            super();
+            this._storage = {};
+        }
+
+        /**
+         * Retrieve value from storage.
+         * @param key The key to lookup.
+         */
+        async get(key: string): Promise<string> {
+            return this._storage[key];
+        }
+
+        /**
+         * Set a value in storage.
+         * @param key They key name to set.
+         * @param value The value to set.
+         */
+        async set(key: string, value: string): Promise<void> {
+            this._storage[key] = value;
+        }
+    };
+}
