f2a3fb5a25f968067bda240fe1f2a74c8ab3a190
bowenwaregames <ttbowen@hotmail.co.uk>
2018-05-20 17:06:03 +0100

    Add extra asserts for level tests.

--- test/managers/levelManager.spec.ts
+++ test/managers/levelManager.spec.ts
@@ -4,19 +4,21 @@ import { LevelManager } from '../../src/client/managers/levelManager';
 describe('LevelManager', () => {
     describe('levelToExp', () => {
         it('should calculate the correct experience from level', () => {
-            const expectedExp = 155;
-            const level = 1;
+            const firstExpectedExp = 155;
+            const secondExpectedExp = 220;
 
-            expect(LevelManager.levelToExp(level)).to.equal(expectedExp);
+            expect(LevelManager.levelToExp(1)).to.equal(firstExpectedExp);
+            expect(LevelManager.levelToExp(2)).to.equal(secondExpectedExp);
         });
     });
 
     describe('getLevelFromExp', () => {
         it('should calculate the correct level from the given experience', () => {
             const expectedLevel = 1;
-            const experience = 155;
+            const secondExpectedLevel = 2;
 
-            expect(LevelManager.getLevelFromExp(experience)).to.equal(expectedLevel);
+            expect(LevelManager.getLevelFromExp(155)).to.equal(expectedLevel);
+            expect(LevelManager.getLevelFromExp(320)).to.equal(secondExpectedLevel);
         });
     });
 });
