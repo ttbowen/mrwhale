f0a8a639f1ad9de5f0d7d2f351bb099347c36c2d
bowenwaregames <ttbowen@hotmail.co.uk>
2018-05-27 21:13:01 +0100

    Add new type definitions for youtube search results.

---
+++ src/types/youtube/youtubeThumbnail.ts
@@ -0,0 +1,5 @@
+export type YouTubeThumbnail = {
+    url: string;
+    width: number;
+    height: number;
+};
---
+++ src/types/youtube/youtubeVideo.ts
@@ -0,0 +1,9 @@
+import { YouTubeId } from './youtubeId';
+import { YouTubeSnippet } from './youtubeSnippet';
+
+export type YouTubeVideo = {
+    kind: string;
+    etag: string;
+    id: YouTubeId;
+    snippet: YouTubeSnippet;
+};
