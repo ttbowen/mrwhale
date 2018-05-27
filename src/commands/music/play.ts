9bcd55cc9a0984ea3dbaa6d3684a1b1ac1e77c4d
bowenwaregames <ttbowen@hotmail.co.uk>
2018-05-27 21:40:55 +0100

    Reimplement play command.

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -1,16 +1,21 @@
 import { TextChannel, VoiceChannel, VoiceConnection } from 'discord.js';
-import { Command, Message } from 'yamdbf';
+import { Command, Message, Util } from 'yamdbf';
 import * as ytdl from 'ytdl-core';
+
 import { BotClient } from '../../client/botClient';
 import { YouTube } from '../../music/YouTube';
+import { PlayOptions } from '../../types/music/playOptions';
+import { Search } from '../../types/music/search';
 import { Track } from '../../types/music/track';
+import { TrackBuilder } from '../../types/music/trackBuilder';
+import { TrackSearch } from '../../types/music/trackSearch';
 
 export default class extends Command<BotClient> {
     constructor() {
         super({
             name: 'play',
             desc: 'play an audio stream from YouTube.',
-            usage: '<prefix>play <url>',
+            usage: '<prefix>play <url|search>',
             group: 'music',
             guildOnly: true,
             aliases: ['p', 'yt']
@@ -20,49 +25,61 @@ export default class extends Command<BotClient> {
     async action(message: Message, args: string[]): Promise<any> {
         const channel: VoiceChannel = message.member.voiceChannel;
         const guildId: string = message.guild.id;
-        let video: string = args.join('');
+        const memberId: string = message.member.id;
+        const videoOrSearch: string = args.join('');
 
         if (!channel) return message.channel.send('You need to join a voice channel first.');
-
-        const youtube: YouTube = new YouTube(this.client);
-
-        if (!ytdl.validateLink(video)) {
-            const msg = (await message.channel.send(
-                ':mag_right: Searching for a video with that name...'
-            )) as Message;
-
-            const search = await youtube.search(video);
-            if (!search.items || search.items.length === 0)
-                return msg.edit('Could not find this video.');
-
-            video = `https://www.youtube.com/watch?v=${search.items[0].id.videoId}`;
-        }
-
-        const videoInfo: ytdl.videoInfo = await this.client.musicPlayer.getVideoInfo(video);
 
         let connection: VoiceConnection;
         if (this.client.musicPlayer.voiceManager.isConnectionOnGuild(message.guild))
             connection = this.client.musicPlayer.voiceManager.getGuildConnection(message.guild);
         else connection = await channel.join();
 
-        const playOptions = {
-            channel: message.channel as TextChannel,
-            voice: connection
-        };
+        if (ytdl.validateLink(videoOrSearch)) {
+            const videoInfo: ytdl.videoInfo = await this.client.musicPlayer.getVideoInfo(
+                videoOrSearch
+            );
+            const playOptions: PlayOptions = {
+                channel: message.channel as TextChannel,
+                voice: connection
+            };
+            const track: Track = new TrackBuilder()
+                .setAuthor(videoInfo.author.name)
+                .setDuration(parseInt(videoInfo.length_seconds, 10))
+                .setIsPlaying(false)
+                .setRequestedBy(message.member)
+                .setThumbail(videoInfo.thumbnail_url)
+                .setTitle(videoInfo.title)
+                .setUrl(videoOrSearch)
+                .build();
 
-        const track: Track = {
-            author: videoInfo.author.name,
-            duration: parseInt(videoInfo.length_seconds, 10),
-            requestedBy: message.member,
-            title: videoInfo.title,
-            url: video,
-            isPlaying: false,
-            thumbnail: videoInfo.thumbnail_url
-        };
+            if (this.client.musicPlayer.streamDispatchers.has(guildId)) {
+                this.client.musicPlayer.playList.add(guildId, track);
+                return message.channel.send(`Added \`${track.title}\` to the playlist.`);
+            } else {
+                this.client.musicPlayer.play(track, playOptions);
+                return message.channel.send(`**Now playing** :notes: \`${track.title}\``);
+            }
+        }
 
-        if (this.client.musicPlayer.streamDispatchers.has(guildId)) {
-            this.client.musicPlayer.playList.add(guildId, track);
-            return message.channel.send(`Added \`${track.title}\` to the playlist.`);
-        } else this.client.musicPlayer.play(track, playOptions);
+        const current: Search = Util.getNestedValue(this.client.musicPlayer.trackSearch.searches, [
+            guildId,
+            memberId
+        ]);
+        if (current && !current.complete) return;
+
+        await this.client.musicPlayer.trackSearch.search(message, videoOrSearch);
+        const search: Search = Util.getNestedValue(this.client.musicPlayer.trackSearch.searches, [
+            guildId,
+            memberId
+        ]);
+
+        let response = '**Please select a track using the `select 1-5` command:**\n';
+        for (let i = 0; i < search.results.length; i++) {
+            response += `**${i + 1}. **`;
+            response += `${search.results[i].title} by ${search.results[i].author}\n`;
+        }
+
+        search.msg.edit(response);
     }
 }
