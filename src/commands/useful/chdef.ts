a1987c613c578d4d539c5e66e00264db200ecd4d
bowenwaregames <ttbowen@hotmail.co.uk>
2018-06-09 21:17:25 +0100

    Prompt users whether they want to add example and restrict to moderators only.

--- src/commands/useful/chdef.ts
+++ src/commands/useful/chdef.ts
@@ -6,6 +6,8 @@ import { BotClient } from '../../client/botClient';
 import { Database } from '../../database/database';
 import { Dictionary } from '../../entity/dictionary';
 import { User } from '../../entity/user';
+import { moderatorOnly } from '../../util/decorators/moderation';
+import { prompt, PromptResult } from '../../util/prompt';
 import { truncate } from '../../util/truncate';
 
 export default class extends Command<BotClient> {
@@ -18,33 +20,56 @@ export default class extends Command<BotClient> {
         });
     }
 
+    @moderatorOnly
     async action(message: Message, args: string[]): Promise<any> {
-        const word: string = args.join(' ');
-        message.channel.send(`Please enter a new definition for ${word}`);
+        const word: string = args.join(' ').toLowerCase();
+        const defMax = 1500;
+        const exampleMax = 400;
+        const time = 20000;
+
+        message.channel.send(`Please enter a new definition for ${word}.`);
 
         const newDef: Message = (await message.channel.awaitMessages(
             a => a.author.id === message.author.id,
-            { max: 1, time: 20000 }
+            { max: 1, time: time }
         )).first();
+
+        if (!newDef) return message.channel.send('Command timed out');
 
         const dictionary: Dictionary = new Dictionary();
-        dictionary.definition = newDef.content;
+        dictionary.definition = truncate(defMax, newDef.content);
         dictionary.word = word;
 
-        message.channel.send(`Please enter an example for ${word}`);
+        const result: PromptResult = await prompt(
+            message,
+            'Enter a new example for this definition? Enter yes or no.',
+            'yes',
+            'no'
+        );
 
-        const newExample: Message = (await message.channel.awaitMessages(
-            a => a.author.id === message.author.id,
-            { max: 1, time: 20000 }
-        )).first();
+        if (result === PromptResult.SUCCESS) {
+            message.channel.send(`Enter an example for ${word}.`);
 
-        dictionary.example = newDef.content;
+            const newExample: Message = (await message.channel.awaitMessages(
+                a => a.author.id === message.author.id,
+                { max: 1, time }
+            )).first();
+
+            if (newExample) dictionary.example = truncate(exampleMax, newExample.content);
+        }
+
+        const foundWord: Dictionary = await Database.connection
+            .getRepository(Dictionary)
+            .findOne({ guildId: message.guild.id, word: word });
+
+        if (foundWord) dictionary.id = foundWord.id;
+
         dictionary.guildId = message.guild.id;
         dictionary.user = new User();
         dictionary.user.id = message.author.id;
 
         Database.connection.getRepository(Dictionary).save(dictionary);
 
-        return message.channel.send('Successfully changed definition');
+        return message.channel.send('Successfully changed definition.');
     }
 }
