fd09197620a0bfd320abaa46496897d787935fac
bowenwaregames <ttbowen@hotmail.co.uk>
2018-06-16 17:54:28 +0100

    If no volume is passed return current volume.

--- src/commands/music/volume.ts
+++ src/commands/music/volume.ts
@@ -2,6 +2,7 @@ import { Collection, StreamDispatcher, VoiceChannel, VoiceConnection } from 'dis
 import { Command, CommandDecorators, Message, Middleware } from 'yamdbf';
 
 import { BotClient } from '../../client/botClient';
+import { MusicManager } from '../../client/managers/musicManager';
 
 const { using } = CommandDecorators;
 const { resolve, expect } = Middleware;
@@ -19,25 +20,28 @@ export default class extends Command<BotClient> {
     }
 
     @using(resolve('volume: Number'))
-    @using(expect('volume: Number'))
     async action(message: Message, [volume]: [number]): Promise<any> {
+        const guildId: string = message.guild.id;
         const channel: VoiceChannel = message.member.voiceChannel;
+        const musicPlayer: MusicManager = this.client.musicPlayer;
         const connection: VoiceConnection = this.client.musicPlayer.voiceManager.getGuildConnection(
             message.guild
         );
 
         if (channel) {
-            if (!this.client.musicPlayer.voiceManager.isOnChannel(channel))
+            if (!musicPlayer.voiceManager.isOnChannel(channel))
                 return message.channel.send('You must be in the same channel first.');
 
-            if (!this.client.musicPlayer.streamDispatchers.has(message.guild.id))
+            if (!musicPlayer.streamDispatchers.has(guildId))
                 return message.channel.send('There is no audio playing.');
 
             try {
-                this.client.musicPlayer.setVolume(message.guild.id, volume);
-                return message.channel.send(
-                    `Set volume to ${this.client.musicPlayer.getVolume(message.guild.id)}%`
-                );
+                if (!volume)
+                    return message.channel.send(`Volume is at ${musicPlayer.getVolume(guildId)}%`);
+
+                musicPlayer.setVolume(guildId, volume);
+
+                return message.channel.send(`Set volume to ${musicPlayer.getVolume(guildId)}%`);
             } catch {
                 return message.channel.send('Could not set the volume.');
             }
